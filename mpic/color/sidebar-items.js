69106f7542b2a578530e1e14ebeacd1628317928
neri <neri@users.noreply.github.com>
2023-07-01 03:24:10 +0000

    deploy: 09b0b020d5978b1a1f7437523b0c7fe68b916a84

---
+++ mpic/color/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"struct":["MpicRgb666","MpicRgb888","MpicYuv666"]};
---
+++ source-files.js
@@ -0,0 +1,4 @@
+var sourcesIndex = JSON.parse('{\
+"mpic":["",[],["chunk.rs","color.rs","decode.rs","encode.rs","lib.rs"]]\
+}');
+createSourceSidebar();
